0b4bfff2aec9830fcce32f1d5be8b5aae6f67d0c
OmEkboteCode <omekbote007@gmail.com>
2026-09-01 18:15:50 +0530

    Add Issue Model And Database Setup

---
+++ models/issue.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const issuesSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  category: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ["Open", "In Progress", "Resolved"],
+    default: "Open",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  image: {
+    filename: {
+      type: String,
+    },
+  },
+});
+
+const Issue = mongoose.model("Issue", issuesSchema);
+
+module.exports = Issue;
